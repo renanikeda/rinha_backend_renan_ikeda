ab64106606732ddcc39e551cb58e6267f55761a5
Renan Ikeda <renan.ikeda@hotmail.com>
2023-09-21 12:18:19 -0300

    Adjust: router

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import router from './router'
 
 const app = express()
 
-const { PORT = 3001, APP = 'quasar-initial', NODE_ENV = 'development', WEB_PORT = 8081 } = process.env
+const { PORT = 3001, APP = 'RINHA-BACKEND-2023', NODE_ENV = 'development', WEB_PORT = 8081 } = process.env
 export const env = NODE_ENV
 console.log(`Env: ${env}`)
 
--- routes/rinha.route.ts
+++ routes/rinha.route.ts
@@ -3,5 +3,8 @@ import { Router } from 'express'
 const router = Router()
 
 router.get('/', async (req, res) => {
+  console.log('Teste GET')
   res.status(200).end()
 })
+
+module.exports = router
