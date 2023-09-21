a779423380557693947dade25c673d334f7d908f
Renan Ikeda <renan.ikeda@hotmail.com>
2023-09-21 12:16:20 -0300

    Adjust: listen express

--- index.ts
+++ index.ts
@@ -1,2 +1,13 @@
 import express = require('express')
+import router from './router'
 
+const app = express()
+
+const { PORT = 3001, APP = 'quasar-initial', NODE_ENV = 'development', WEB_PORT = 8081 } = process.env
+export const env = NODE_ENV
+console.log(`Env: ${env}`)
+
+!(async function () {
+  app.use(router)
+  app.listen(PORT, () => console.log(`Environment : ${env} - Api ${APP} running on port ${PORT}!`))
+})()
