5b3810ddaf1ee260849c54fa0beb3e137d407931
Renan Ikeda <renan.ikeda@hotmail.com>
2023-09-21 12:01:15 -0300

    Add: route

---
+++ router.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express'
+import * as glob from 'glob'
+import { relative, dirname } from 'path'
+
+const router = Router()
+
+glob(`${__dirname}/**/*.route.{ts,js}`, (err: Error, files: string[]) => {
+	if (!err) {
+		files.forEach(file => {
+			const endpoint = relative(__dirname, dirname(file))
+			console.log(`Iniciando rotas ${file}`)
+			// eslint-disable-next-line @typescript-eslint/no-var-requires
+			router.use(`/api/${endpoint}`, require(file))
+		})
+	}
+})
+
+
+export default router
