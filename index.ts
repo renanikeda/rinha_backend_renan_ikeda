9812ebc228a6b1171ee4ae86c487f2bae9eff6dd
Renan Ikeda <renan.ikeda@hotmail.com>
2023-09-22 17:18:10 -0300

    Add: db url in index

--- index.ts
+++ index.ts
@@ -3,9 +3,13 @@ import router from './router'
 
 const app = express()
 
-const { PORT = 8080, APP = 'RINHA-BACKEND-2023', NODE_ENV = 'development' } = process.env
+const { PORT = 8080, APP = 'RINHA-BACKEND-2023', NODE_ENV = 'development', DB_URL = "postgres://user:pass@postgres:5555/postgres" } = process.env
+
 export const env = NODE_ENV
+export const db_url = DB_URL
+
 console.log(`Env: ${env}`)
+console.log(`DB URL: ${db_url}`)
 
 !(async function () {
   app.use(router)
