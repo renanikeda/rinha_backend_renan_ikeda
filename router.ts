72ca8cf9addf1040e90311bb6e087fc972bb5c07
Renan Ikeda <renan.ikeda@hotmail.com>
2023-09-21 22:51:38 -0300

    Adjust: simplifying routes

--- router.ts
+++ router.ts
@@ -1,19 +1,8 @@
 import { Router } from 'express'
-import * as glob from 'glob'
-import { relative, dirname } from 'path'
 
 const router = Router()
 
-glob(`${__dirname}/**/*.route.{ts,js}`, (err: Error, files: string[]) => {
-	if (!err) {
-		files.forEach(file => {
-			const endpoint = relative(__dirname, dirname(file))
-			console.log(`Iniciando rotas ${file}`)
-			// eslint-disable-next-line @typescript-eslint/no-var-requires
-			router.use('/', require(file))
-		})
-	}
-})
-
-
+const file = `${__dirname}/routes/rinha.route.js`
+console.log(`Iniciando rotas ${file}`)
+router.use('/', require(file))
 export default router
