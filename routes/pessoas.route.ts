1dc1e86b9256f10d0d1e32644c6e0751ff0a5204
Renan Ikeda <renan.ikeda@hotmail.com>
2023-09-24 10:10:05 -0300

    Add: valida body

--- routes/pessoas.route.ts
+++ routes/pessoas.route.ts
@@ -1,10 +1,44 @@
 import { Router } from 'express'
 import { inserirPessoa } from '../database/database'
 const { v4: uuidv4 } = require('uuid')
-
+const { parse } = require('date-fns');
 const router = Router()
 
-router.post('/', async (req, res) => {
+
+const validaBody = (req, res, next) => {
+  const validaData = (dateString) => {
+    return !isNaN(parse(dateString, 'yyyy-MM-dd', new Date()))
+  }
+
+  function valida(req) {
+    const { apelido, nome, nascimento, stack } = req.body
+
+    if(typeof apelido !== 'string' || apelido.length > 32)
+      return false
+
+    if(typeof nome !== 'string' || nome.length > 100)
+      return false
+
+    if(typeof nascimento !== 'string' || !validaData(nascimento))
+      return false
+
+    if(stack !== undefined && !Array.isArray(stack))
+      req.body.stack = []
+
+    if(stack && stack.length)
+      req.body.stack = stack.filter((s) => typeof s !== 'string' || s === "" || s.length > 32)
+
+    return true
+  }
+
+  if (!valida(req))
+      return res.status(422).end()
+
+  next()
+}
+
+
+router.post('/', validaBody, async (req, res) => {
   console.log('Route POST /pessoas')
   const { apelido, nome, nascimento, stack } = req.body
   const id = uuidv4()
